610afef2ded2d121bf3cf6449d968e6a643ec607
Max Tedford <max.tedford@gmail.com>
2015-07-27 11:21:13 -0600

    create a users api controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :items, except: [:new, :edit]
+      resources :users, except: [:new, :edit]
     end
   end
 end
