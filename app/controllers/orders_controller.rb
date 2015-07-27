f96c41a8fd99f2443a7d1e739c0b30f9c62c26d0
Max Tedford <max.tedford@gmail.com>
2015-07-27 09:41:53 -0600

    remove instance-level and leave only the class-level method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,23 +4,11 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
     
-    respond_to do |format|
-      format.html { @orders }
-      format.json { render json: @orders }
-      format.xml  { render xml:  @orders }
-    end
-    
     respond_with @orders
   end
 
   def show
     @order = Order.find(params[:id])
-    
-    respond_to do |format|
-      format.html { @orders }
-      format.json { render json: @order }
-      format.xml  { render xml:  @order }
-    end
     
     respond_with @order
   end
