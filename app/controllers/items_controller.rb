11d196b7f2e82589ddf2ad616b3a32e694dc4b7e
Max Tedford <max.tedford@gmail.com>
2015-07-27 10:22:23 -0600

    add the same logic to the update method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,10 +46,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
 
     if @item.update_attributes(item_params)
-      redirect_to item_path(@item), notice: "The item was updated."
+      respond_to do |format|
+        format.html { redirect_to item_path(@item), notice: "The item was updated." }
+        format.json { render json: @item }
+        format.xml  { render xml:  @item }
+      end
     else
-      flash.now[:notice] = "The item was not updated."
-      render :edit
+      respond_to do |format|
+        format.html do
+          flash.now[:notice] = "The item was not created."
+          render :edit
+        end
+        format.json { render json: { messages: @item.errors.messages } }
+        format.xml  { render xml:  { messages: @item.errors.messages } }
+      end
     end
   end
 
