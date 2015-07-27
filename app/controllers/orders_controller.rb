f72322ec3ee15ebc0d9366de0c7b3f7667e6ead1
Max Tedford <max.tedford@gmail.com>
2015-07-27 09:39:26 -0600

    update orders controller with instance-level respond_to and class-level respond_to

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,27 @@
 class OrdersController < ApplicationController
+  respond_to :html, :json, :xml
+  
   def index
     @orders = Order.all
+    
+    respond_to do |format|
+      format.html { @orders }
+      format.json { render json: @orders }
+      format.xml  { render xml:  @orders }
+    end
+    
+    respond_with @orders
   end
 
   def show
     @order = Order.find(params[:id])
+    
+    respond_to do |format|
+      format.html { @orders }
+      format.json { render json: @order }
+      format.xml  { render xml:  @order }
+    end
+    
+    respond_with @order
   end
 end
